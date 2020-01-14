b3a45c7457cff510765c08c39c5dfc037435c93e
josefrangl <josefrangl@me.com>
2020-01-14 09:32:07 +0100

    backend sequelize postgres test

--- db.js
+++ db.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect:  'postgres',
+  protocol: 'postgres',
+  port:     match[4],
+  host:     match[3],
+  logging:  true //false
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 exports.getData = function () {
   return tempData;
 }
