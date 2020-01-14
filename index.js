525ff504cefc319c8a7cdf17fca894704032116b
josefrangl <josefrangl@me.com>
2020-01-14 14:12:33 +0100

    final fix ? create users v2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('koa-bodyparser');
 
 const router = require('./router');
 
-const db = require('./models/index');
+const db = require('./models/index.js');
 
 app.use(bodyParser());
 app.use(router.routes());
